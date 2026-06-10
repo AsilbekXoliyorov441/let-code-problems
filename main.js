2ef4e55cc94bf04f3cbdd5f7b4d0ede816e314cd
AsilbekXoliyorov441 <gfdhghj>
2026-06-10 23:50:32 +0500

    counter

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-var twoSum = function (nums, target) {
-    var sum = 0;
-    var leng = nums.length - 1;
-    for (var i in nums) {
-        for (var j = +i + 1; j <= leng; j++) {
-            sum = nums[i] + nums[j];
-            if (sum === target) {
-                return console.log([+i, j]);
-            }
-        }
-    }
+//two sum
+// var twoSum = function (nums:number[], target:number) {
+//     let sum = 0;
+//     let leng = nums.length - 1
+//     for (let i in nums) {
+//         for (let j = +i+1; j <= leng; j++) {
+//             sum = nums[i] + nums[j]
+//             if (sum === target) {
+//                 return console.log([+i, j]);
+//             }
+//         }
+//     }
+// };
+// twoSum([2,5,5,11], 10);
+//counter
+var createCounter = function (n) {
+    var count = n;
+    return function () {
+        return count++;
+    };
 };
-twoSum([2, 5, 5, 11], 10);
+var counter = createCounter(10);
+console.log(counter);
--- main.ts
+++ main.ts
@@ -1,18 +1,36 @@
+//two sum
+// var twoSum = function (nums:number[], target:number) {
+//     let sum = 0;
+//     let leng = nums.length - 1
+//     for (let i in nums) {
 
-var twoSum = function (nums:number[], target:number) {
-    let sum = 0;
-    let leng = nums.length - 1
-    for (let i in nums) {
+//         for (let j = +i+1; j <= leng; j++) {
+//             sum = nums[i] + nums[j]
 
-        for (let j = +i+1; j <= leng; j++) {
-            sum = nums[i] + nums[j]
+//             if (sum === target) {
+//                 return console.log([+i, j]);
+//             }
+//         }
+//     }
+// };
 
-            if (sum === target) {
-                return console.log([+i, j]);
-            }
-        }
-    }
+
+// twoSum([2,5,5,11], 10);
+
+
+
+//counter
+
+
+var createCounter = function(n:number) {
+    let count = n;   
+    return function(){
+      return count++
+    };
 };
 
 
-twoSum([2,5,5,11], 10);
+const counter = createCounter(10);
+
+
+
